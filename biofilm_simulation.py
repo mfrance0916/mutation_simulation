b51cf748dbaa6283de5107b8b88ec4f8bd075d43
Michael <michael.t.france@gmail.com>
2015-05-04 13:06:45 -0700

    added initial python script, work in progress

---
+++ biofilm_simulation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+class population:
+	def __init__(self,length,width,height,n_iters):
+		self.width = width
+		self.length = length
+		self.height = height
+		self.n_iters = n_iters
+
+	def start(self):
+
+	def update(self):
+
+	def evaluate(self):
+
+
+	def plot(self):
